7c81bf10886c0182eee0eb8c297cb12868e7d76c
hello-cjlee-s-world <cndwn2131@naver.com>
2024-01-11 22:34:28 +0900

    어노테이션 완료

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,6 +1,23 @@
 package polymorphism;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTV implements TV{
+	// 어노테이션 의존성 주입 1. 
+	// Autowired는 변수의 타입(이 클래스에서는 Speaker)을 기준으로 객체를 탐색하여 의존성 주입을 한다.
+	 @Autowired
+	//@Qualifier("apple")
+	// 어노테이션 의존성 주입 2. 
+	// Autowired는 객체의 이름(apple --> applicationContext.xml에 apple 이라고 설정해놓았다)
+	// 을 기준으로 객체를 탐색하여 의존성 주입을 한다.
+	//@Resource(name="sony")
+	private Speaker speaker;
+	
 	public LgTV() {
 		System.out.println("======> LgTV 객체 생성");
 	}
@@ -14,10 +31,12 @@ public class LgTV implements TV{
 	}
 	 @Override
 	public void volumeUp() {
-		System.out.println("LgTV--볼륨을 높입니다.");
+		 speaker.volumeUp();
+		//System.out.println("LgTV--볼륨을 높입니다.");
 	}
 	 @Override
 	public void volumeDown() {
-		System.out.println("LgTV--볼륨을 낮춥니다..");
+		 speaker.volumeDown();
+		//System.out.println("LgTV--볼륨을 낮춥니다..");
 	}
 }
