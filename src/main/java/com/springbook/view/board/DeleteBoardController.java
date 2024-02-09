8608d2dc30ab40284e9dc3dab681afbd17bd425c
hello-cjlee-s-world <cndwn2131@naver.com>
2024-02-10 02:14:22 +0900

    Spring MVC 적용

--- src/main/java/com/springbook/view/board/DeleteBoardController.java
+++ src/main/java/com/springbook/view/board/DeleteBoardController.java
@@ -3,14 +3,16 @@ package com.springbook.view.board;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDao;
-import com.springbook.view.controller.Controller;
 
 public class DeleteBoardController implements Controller {
 
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 삭제 처리");
 		// 1. seq 정보 추출
 		int seq = Integer.parseInt(request.getParameter("seq"));
@@ -20,7 +22,9 @@ public class DeleteBoardController implements Controller {
 		new BoardDao().deleteBoard(vo);
 
 		// 3. 화면 네비게이션
-		return "getBoardList.do";
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:getBoardList.do");
+		return mav;
 	}
 
 }
